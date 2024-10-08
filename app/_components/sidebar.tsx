e03dff920fcab342a58992c8ab8dabd8fffe0386
Tamiris Cristine <tamiris.css@gmail.com>
2024-10-08 13:03:34 +0200

    feat: add initial pages

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,8 +1,14 @@
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
-      <p>This is the sidebar</p>
+    <div className="w-64 bg-white">
+      <div className="px-8 py-6">
+        <h1 className="text-2xl font-bold">STOCKLY</h1>
+      </div>
+      <div className="flex flex-col gap-2 p-2">
+        <button className="px-6 py-3">Dashboard</button>
+        <button className="px-6 py-3">Products</button>
+        <button className="px-6 py-3">Sales</button>
+      </div>
     </div>
   );
 };
