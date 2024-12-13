e5be9a48b1e737aee692a487becf93ad9a4acfca
Tamiris Cristine <tamiris.css@gmail.com>
2024-12-13 14:52:15 +0100

    feat: add initial sale creation component

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,42 @@
-const SalesPage = () => {
-  return <div>sales page</div>;
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { ComboboxOption } from "../_components/ui/combobox";
+import { Sheet, SheetTrigger } from "../_components/ui/sheet";
+import { getProduct } from "../_data-access/product/get-product";
+import UpsertSheetContent from "./_components/upsert-sheet-content";
+const SalesPage = async () => {
+  const products = await getProduct();
+
+  const productOptions: ComboboxOption[] = products.map((product) => ({
+    label: product.name,
+    value: product.id,
+  }));
+
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Sales Overview
+          </span>
+          <h2 className="text-xl font-semibold">Sales</h2>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="gap-2">
+              <PlusIcon size={20} />
+              New sale
+            </Button>
+          </SheetTrigger>
+          <UpsertSheetContent
+            products={products}
+            productOptions={productOptions}
+          />
+        </Sheet>
+      </div>
+      {/* <DataTable columns={productTableColumns} data={products} /> */}
+    </div>
+  );
 };
 
 export default SalesPage;
