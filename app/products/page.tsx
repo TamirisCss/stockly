e69ebd42b5ef01e645b0d11bae9db8b562c097a8
Tamiris Cristine <tamiris.css@gmail.com>
2024-12-19 15:12:02 +0100

    feat: add header component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,21 +2,27 @@ import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
 import { getProducts } from "../_data-access/product/get-products";
 import CreateProductButton from "./_components/create-product-button";
+import Header, {
+  HeaderLeft,
+  HeaderRight,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "../_components/header";
 
 const ProductsPage = async () => {
   const products = await getProducts();
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
-      <div className="flex w-full items-center justify-between">
-        <div className="space-y-1">
-          <span className="text-xs font-semibold text-slate-500">
-            Products list
-          </span>
-          <h2 className="text-xl font-semibold">Products</h2>
-        </div>
-        <CreateProductButton />
-      </div>
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Product List</HeaderSubtitle>
+          <HeaderTitle>Products</HeaderTitle>
+        </HeaderLeft>
+        <HeaderRight>
+          <CreateProductButton />
+        </HeaderRight>
+      </Header>
       <DataTable columns={productTableColumns} data={products} />
     </div>
   );
