bcb83e08e36032e286d33a84d152f7820465c697
Tamiris Cristine <tamiris.css@gmail.com>
2025-01-08 12:18:23 +0100

    feat: add total sales component with streaming

---
+++ app/_data-access/dashboard/get-total-sales.ts
@@ -0,0 +1,9 @@
+import "server-only";
+
+import { db } from "@/app/_lib/prisma";
+
+export const getTotalSales = async (): Promise<number> => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return db.sale.count();
+};
