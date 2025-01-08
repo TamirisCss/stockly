d990a429c6c89970daf3aa9f02c6486b5d408a1f
Tamiris Cristine <tamiris.css@gmail.com>
2025-01-08 13:35:08 +0100

    feat: add last 14 days revenue card with streaming

--- app/(dashboard)/_components/revenue-chart.tsx
+++ app/(dashboard)/_components/revenue-chart.tsx
@@ -5,7 +5,8 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/app/_components/ui/chart";
-import { DayTotalRevenue } from "@/app/_data-access/dashboard/get-dashboard";
+import { DayTotalRevenueDto } from "@/app/_data-access/dashboard/get-last-14-days-revenue";
+
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 const chartConfig = {
   totalRevenue: {
@@ -13,7 +14,7 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 interface RevenueChartProps {
-  data: DayTotalRevenue[];
+  data: DayTotalRevenueDto[];
 }
 const RevenueChart = ({ data }: RevenueChartProps) => {
   return (
--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -1,7 +1,6 @@
 import "server-only";
 
 import { db } from "@/app/_lib/prisma";
-import dayjs from "dayjs";
 import { ProductStatusDto } from "../product/get-products";
 
 export interface MostSoldProductDto {
@@ -12,42 +11,11 @@ export interface MostSoldProductDto {
   price: number;
 }
 
-export interface DayTotalRevenue {
-  day: string;
-  totalRevenue: number;
-}
 interface DashboardDto {
-  totalLast14DaysRevenue: DayTotalRevenue[];
   mostSoldProducts: MostSoldProductDto[];
 }
 
 export const getDashboard = async (): Promise<DashboardDto> => {
-  const today = dayjs().endOf("day").toDate();
-  const last14Days = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(
-    (day) => {
-      return dayjs(today).subtract(day, "day");
-    },
-  );
-  const totalLast14DaysRevenue: DayTotalRevenue[] = [];
-  for (const day of last14Days) {
-    const dayTotalRevenue = await db.$queryRawUnsafe<
-      { totalRevenue: number }[]
-    >(
-      `
-      SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "totalRevenue"
-      FROM "SaleProduct"
-      JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
-      WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2;
-    `,
-      day.startOf("day").toDate(),
-      day.endOf("day").toDate(),
-    );
-    totalLast14DaysRevenue.push({
-      day: day.format("DD/MM"),
-      totalRevenue: dayTotalRevenue[0].totalRevenue,
-    });
-  }
-
   const mostSoldProductsQuery = `
   SELECT "Product"."name", SUM("SaleProduct"."quantity") as "totalSold", "Product"."price", "Product"."stock", "Product"."id" as "productId"
   FROM "SaleProduct"
@@ -69,7 +37,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
   const [mostSoldProducts] = await Promise.all([mostSoldProductsPromise]);
 
   return {
-    totalLast14DaysRevenue,
     mostSoldProducts: mostSoldProducts.map((product) => ({
       ...product,
       totalSold: Number(product.totalSold),
