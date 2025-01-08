533ef10f6e240b1c8856229ff2369f1873049248
Tamiris Cristine <tamiris.css@gmail.com>
2025-01-08 13:17:34 +0100

    feat: add total in stock card component with streaming

---
+++ app/(dashboard)/_components/total-in-stock-card.tsx
@@ -0,0 +1,24 @@
+import { getTotalInStock } from "@/app/_data-access/dashboard/get-total-in-stock";
+import {
+  SummaryCard,
+  SummaryCardIcon,
+  SummaryCardTitle,
+  SummaryCardValue,
+} from "./summary-card";
+import { PackageIcon } from "lucide-react";
+
+const TotalInStockCard = async () => {
+  const totalInStock = await getTotalInStock();
+
+  return (
+    <SummaryCard>
+      <SummaryCardIcon>
+        <PackageIcon />
+      </SummaryCardIcon>
+      <SummaryCardTitle>Total in Stock</SummaryCardTitle>
+      <SummaryCardValue>{totalInStock}</SummaryCardValue>
+    </SummaryCard>
+  );
+};
+
+export default TotalInStockCard;
