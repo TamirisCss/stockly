d1c3e17f4cf6523b8cdd4a0a7b9d66a9ba69dac8
Tamiris Cristine <tamiris.css@gmail.com>
2025-01-08 12:09:39 +0100

    feat: add today revenue component with streaming

---
+++ app/(dashboard)/_components/today-revenue-card.tsx
@@ -0,0 +1,25 @@
+import {
+  SummaryCard,
+  SummaryCardIcon,
+  SummaryCardTitle,
+  SummaryCardValue,
+} from "./summary-card";
+import { DollarSign } from "lucide-react";
+import { formatCurrency } from "@/app/_helpers/currency";
+import { getTodayRevenue } from "@/app/_data-access/dashboard/get-today-revenue";
+
+const TodayRevenueCard = async () => {
+  const todayRevenue = await getTodayRevenue();
+
+  return (
+    <SummaryCard>
+      <SummaryCardIcon>
+        <DollarSign />
+      </SummaryCardIcon>
+      <SummaryCardTitle>Today Revenue</SummaryCardTitle>
+      <SummaryCardValue>{formatCurrency(todayRevenue)}</SummaryCardValue>
+    </SummaryCard>
+  );
+};
+
+export default TodayRevenueCard;
